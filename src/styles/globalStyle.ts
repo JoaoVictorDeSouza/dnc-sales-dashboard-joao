b8b4d86ad64815dfe1f6084802f8a3f86f16feb0
Joao Victor de Souza <73joao.victor@gmail.com>
2025-07-03 23:15:43 -0300

    Feat: Corrected syntax

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,15 +1,15 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from 'styled-components';
+import type { DefaultTheme } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     body, html {
-        background: ${(props) => props.theme.appBackground};
+        background-color: ${(props) => props.theme.appBackground};
         color: ${(props) => props.theme.appColor};
+        font-family: "Inter", sans-serif;
         margin: 0;
         padding: 0;
-        font-family: "Inter", sans-serif;
     }
-    
-    h1, h2, p, ul, li {
+    h1, h2, p, ul, li, figure {
         margin: 0;
         padding: 0;
     }
