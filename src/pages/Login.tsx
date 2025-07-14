a847daa2865c23b1e83a2e70ab288ca4332a8db2
Joao Victor de Souza <73joao.victor@gmail.com>
2025-07-13 21:55:23 -0300

    Feat: Form component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,46 @@
-import { Box, Container, Grid } from "@mui/material";
-import { BannerImage } from "@/components";
+import { Box, Container, Grid } from '@mui/material'
+import { BannerImage, FormComponent, StyledH1, StyledP } from '@/components'
+import { pxToRem } from '@/utils'
 
 function Login() {
   return (
     <>
       <Box>
         <Grid container>
-          <Grid 
+          <Grid
             container
             sx={{
               flexDirection: { xs: 'column', sm: 'row' }, // exemplo de responsividade
               alignItems: 'center',
               display: 'flex',
               height: '100vh',
-              width: '50vw'
-              // aqui você pode definir tamanhos usando flexbox ou outras propriedades
+              width: '50vw', // aqui você pode definir tamanhos usando flexbox ou outras propriedades
             }}
           >
             <Container maxWidth="sm">
-              <h1>LOGIN</h1>
+              <Box sx={{ marginBottom: pxToRem(24) }}>logo</Box>
+              <Box sx={{ marginBottom: pxToRem(24) }}>
+                <StyledH1>Bem-vindo</StyledH1>
+                <StyledP>Digite sua senha para logar</StyledP>
+              </Box>
+              <FormComponent
+                inputs={[
+                  { type: 'email', placeholder: 'Email' },
+                  { type: 'password', placeholder: 'Senha' },
+                ]}
+                buttons={[
+                  {
+                    className: 'primary',
+                    type: 'submit',
+                    children: 'Login',
+                    disabled: true,
+                  },
+                ]}
+                message={{
+                  msg: 'Sucesso!!!',
+                  type: 'success',
+                }}
+              />
             </Container>
           </Grid>
           <Grid
@@ -32,7 +54,7 @@ function Login() {
         </Grid>
       </Box>
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,24 +1,53 @@
-import { Box, Container, Grid } from "@mui/material";
-import { BannerImage } from "@/components";
+import { Box, Container, Grid } from '@mui/material'
+import {
+  BannerImage,
+  FormComponent,
+  StyledH1,
+  StyledP,
+  StyledUl,
+} from '@/components'
 
 function Registration() {
   return (
     <>
       <Box>
         <Grid container>
-          <Grid 
+          <Grid
             container
             sx={{
               flexDirection: { xs: 'column', sm: 'row' }, // exemplo de responsividade
               alignItems: 'center',
               display: 'flex',
               height: '100vh',
-              width: '50vw'
+              width: '50vw',
               // aqui você pode definir tamanhos usando flexbox ou outras propriedades
             }}
           >
             <Container maxWidth="sm">
-              <h1>CADASTRO</h1>
+              <Box>
+                <StyledH1>Faça o seu cadastro</StyledH1>
+                <StyledP>Primeiro, diga-nos quem você é</StyledP>
+                <StyledUl>
+                  <li>Pelo menos 8 caracteres</li>
+                  <li>Pelo menos uma letra maiúscula</li>
+                  <li>Pelo menos um caractere especial</li>
+                  <li>Pelo menos um número</li>
+                </StyledUl>
+              </Box>
+
+              <FormComponent
+                inputs={[
+                  { type: 'email', placeholder: 'Email' },
+                  { type: 'password', placeholder: 'Senha' },
+                ]}
+                buttons={[
+                  { className: 'primary', type: 'submit', children: 'Login' },
+                ]}
+                message={{
+                  msg: 'ERRO!!!',
+                  type: 'error',
+                }}
+              />
             </Container>
           </Grid>
           <Grid
